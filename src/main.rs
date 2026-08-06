f346e534955c035c0505094b3012eedfaf7b9d43
Aniketh Dandu <aniketh.dandu@icloud.com>
2026-08-06 01:22:26 -0700

    Add timer reads to create blink program

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,13 @@ pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 // Halts processor on panic
 use cortex_m_rt::entry;
 use panic_halt as _;
-use rp2040_pac;
+use rp2040_pac::{self};
 
 // entry point after RAM initialization
 // NOTE: never ending function, returns !
 #[entry]
 fn main() -> ! {
-    let mut peripherals = unsafe { rp2040_pac::Peripherals::steal() };
+    let peripherals = unsafe { rp2040_pac::Peripherals::steal() };
 
     // De-assert RESETs
     peripherals.RESETS.reset().modify(|_, w| {
@@ -45,5 +45,32 @@ fn main() -> ! {
         .SIO
         .gpio_out_set()
         .write(|w| unsafe { w.bits(1 << 25) });
-    loop {}
+
+    let mut timer_high: u32;
+    let mut next_high: u32;
+    let mut timer_low: u32;
+    let mut prev_time: u64 = 0;
+    let mut time: u64;
+
+    loop {
+        timer_high = peripherals.TIMER.timerawh().read().bits();
+        timer_low = peripherals.TIMER.timerawl().read().bits();
+        next_high = peripherals.TIMER.timerawh().read().bits();
+
+        while timer_high != next_high {
+            timer_high = next_high;
+            timer_low = peripherals.TIMER.timerawl().read().bits();
+            next_high = peripherals.TIMER.timerawh().read().bits();
+        }
+
+        time = ((timer_high as u64) << 32) | (timer_low as u64);
+
+        if (time - prev_time) > 250000 {
+            prev_time = time;
+            peripherals
+                .SIO
+                .gpio_out_xor()
+                .write(|w| unsafe { w.bits(1 << 25) });
+        }
+    }
 }
